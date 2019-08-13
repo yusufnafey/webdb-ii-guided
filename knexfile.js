c59c074db42ca82a5513c3a56042d7dfa70d6b8c
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-13 10:31:51 -0700

    runs migrations

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
     client: "sqlite3",
     connection: {
       filename: "./dev.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
     }
   }
 };
