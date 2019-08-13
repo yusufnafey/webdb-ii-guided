660379b650c55fd8d623bec8762e23687570372b
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-13 10:52:05 -0700

    creates table and runs migrations again

--- data/migrations/20190813103128_fruits_table.js
+++ data/migrations/20190813103128_fruits_table.js
@@ -1,3 +1,16 @@
-exports.up = function(knex) {};
+exports.up = function(knex) {
+  // primary key, called id, integer, autoincrements
+  return knex.schema.createTable("fruits", tbl => {
+    tbl.increments();
+    tbl
+      .string("name", 128)
+      .unique()
+      .notNullable();
+    tbl.decimal("avg-weight-oz"); //, (characters, 8 default), (decimal place, 2 default)
+  });
+};
 
-exports.down = function(knex) {};
+// how we undo the changes made in the up function
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExits("fruits");
+};
